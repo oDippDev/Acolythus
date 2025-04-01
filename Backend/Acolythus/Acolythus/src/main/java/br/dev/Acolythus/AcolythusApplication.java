68529dd1dca256eda7e9d047791a9b0304a71525
oDippDev <carlosmanoel.dev@gmail.com>
2025-04-01 20:52:25 -0300

    chore: configura e inicializa o Spring no projeto

---
+++ Backend/Acolythus/Acolythus/src/main/java/br/dev/Acolythus/AcolythusApplication.java
@@ -0,0 +1,13 @@
+package br.dev.Acolythus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcolythusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcolythusApplication.class, args);
+	}
+
+}
---
+++ Backend/Acolythus/Acolythus/src/test/java/br/dev/Acolythus/AcolythusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.Acolythus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcolythusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
