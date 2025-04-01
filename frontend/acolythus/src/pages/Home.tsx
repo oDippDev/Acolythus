ff87a6efd0e1c01ae7492b4bd07264f474b99055
Israel de Souza <israellimas@hotmail.com>
2025-04-01 19:26:42 -0300

    feat: create page initial

--- frontend/acolythus/src/pages/Home.tsx
+++ frontend/acolythus/src/pages/Home.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import style from "./styles/home.module.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={style.home__background}>
+      <div className={style.home__box}>
+        <div>
+          <input
+            className={style.home__input}
+            type="password"
+            placeholder="Digite a sua senha aqui"
+          />
+        </div>
+        <div className={style['home__btn-link']}>
+          <div>
+            <input className={style.home__btn} type="button" value="ENTRAR" />
+          </div>
+          <div>
+            <Link className={style.home__link} to={"/esqueci-senha"}>Esqueceu a senha?</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export { Home };
